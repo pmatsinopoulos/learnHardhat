92d461b9a602f8af423b1521bc8b47cea5ba7a91
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-06-09 09:27:43 +0300

    Hardhat configuration to declare the network 'sepolia'

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+// Ensure your configuration variables are set before executing the script
+const { vars } = require("hardhat/config");
+
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
