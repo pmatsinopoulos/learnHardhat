6e845f1dd573f7a599728859a319d08355469f95
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-06-13 00:36:26 +0300

    Added more tests

--- openzeppelin/test/contracts/Box.test.js
+++ openzeppelin/test/contracts/Box.test.js
@@ -2,10 +2,18 @@
 
 const { expect } = require("chai");
 
+require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-chai-matchers");
+
 // Start test block
 describe("Box", function () {
+  const value = 42n;
+  let owner;
+  let other;
+
   before(async function () {
     this.Box = await ethers.getContractFactory("Box");
+    [owner, other] = await ethers.getSigners();
   });
 
   beforeEach(async function () {
@@ -21,5 +29,17 @@ describe("Box", function () {
     // Test if the returned value is the same one
     // Note that we need to use strings to compare the 256 bit integers
     expect((await this.box.retrieve()).toString()).to.equal("42");
+  });
+
+  it("store emits an event", async function () {
+    await expect(this.box.store(value))
+      .to.emit(this.box, "ValueChanged")
+      .withArgs(value);
+  });
+
+  it("non owner cannot store a value", async function () {
+    await expect(this.box.connect(other).store(value))
+      .to.be.revertedWithCustomError(this.box, "OwnableUnauthorizedAccount")
+      .withArgs(other.address);
   });
 });
