eba3d351d68418f77eff3163dd6166b054847aa9
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-06-12 08:23:05 +0300

    Use OpenZeppelin Ownable contract

--- openzeppelin/contracts/Box.sol
+++ openzeppelin/contracts/Box.sol
@@ -1,25 +1,17 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.24;
 
-import "./access-control/Auth.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Box {
+contract Box is Ownable {
   uint256 private _value;
-  Auth private _auth;
 
   event ValueChanged(uint256 value);
 
-  constructor() {
-    _auth = new Auth(msg.sender);
-  }
+  constructor() Ownable(msg.sender) {}
 
 
-  modifier authenticate {
-    require(_auth.authenticate(msg.sender), "You are not authenticated to use Box");
-    _;
-  }
-
-  function store(uint256 value) public authenticate {
+  function store(uint256 value) public onlyOwner {
     _value = value;
 
     emit ValueChanged(value);
