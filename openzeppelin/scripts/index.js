6efd041c310cd2c970f29425acbd96afa01bc96a
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-06-12 09:25:21 +0300

    Get contract instance, call the contract, send a transaction

--- openzeppelin/scripts/index.js
+++ openzeppelin/scripts/index.js
@@ -10,6 +10,27 @@ const main = async () => {
   // const accounts = await ethers.provider.listAccounts();
 
   console.log(accounts);
+
+  // Getting a contract instance
+
+  const address = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+  const Box = await ethers.getContractFactory("Box");
+  const box = await Box.attach(address);
+
+  // Calling the contract
+
+  let value = await box.retrieve();
+  console.log("value = ", value);
+
+  // Sending a transaction
+
+  // In a real-world application, I may want to estimate the gas of my
+  // transaction, and check a gas price oracle to know the optimal values
+  // to use on every transaction.
+  await box.store(23);
+
+  value = await box.retrieve();
+  console.log(value);
 };
 
 main()
